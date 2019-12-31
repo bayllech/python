7c0ed6c11faccd0e006a26c7b56fb1bccdfc38b5
bayllech <hpuqibei@163.com>
2019-12-31 18:33:38 +0800

    over day11

---
+++ base/100Days/day10/io.py
@@ -0,0 +1,108 @@
+import time
+from math import sqrt
+import json
+import  requests
+
+
+def read_txt():
+    # 一次性读取整个文件内容
+    with open('致橡树.txt', 'r', encoding='utf-8') as f:
+        print(f.read())
+
+    # 通过for-in循环逐行读取
+    with open('致橡树.txt', mode='r', encoding='utf-8') as f:
+        for line in f:
+            print(line, end='')
+            time.sleep(0.5)
+    print()
+
+    # 读取文件按行读取到列表中
+    with open('致橡树.txt', encoding='utf-8') as f:
+        lines = f.readlines()
+    print(lines)
+
+
+def is_prime(n):
+    """判断素数的函数"""
+    assert n > 0
+    for factor in range(2, int(sqrt(n)) + 1):
+        if n % factor == 0:
+            return False
+    return True if n != 1 else False
+
+
+def prime():
+    filenames = ('a.txt', 'b.txt', 'c.txt')
+    fs_list = []
+    try:
+        for filename in filenames:
+            fs_list.append(open(filename, 'w', encoding='utf-8'))
+        for number in range(1, 10000):
+            if is_prime(number):
+                if number < 100:
+                    fs_list[0].write(str(number) + '\n')
+                elif number < 1000:
+                    fs_list[1].write(str(number) + '\n')
+                else:
+                    fs_list[2].write(str(number) + '\n')
+    except IOError as ex:
+        print(ex)
+        print('写文件时发生错误!')
+    finally:
+        for fs in fs_list:
+            fs.close()
+    print('操作完成!')
+
+
+def pic():
+    try:
+        with open('pic.jpg', 'rb') as fs1:
+            data = fs1.read()
+            print(type(data))  # <class 'bytes'>
+        with open('克隆.jpg', 'wb') as fs2:
+            fs2.write(data)
+    except FileNotFoundError as e:
+        print('指定的文件无法打开.')
+    except IOError as e:
+        print('读写文件时出现错误.')
+    print('程序执行结束.')
+
+
+def json_test():
+    mydict = {
+        'name': '明明',
+        'age': 38,
+        'qq': 957658,
+        'friends': ['王大锤', '白元芳'],
+        'cars': [
+            {'brand': 'BYD', 'max_speed': 180},
+            {'brand': 'Audi', 'max_speed': 280},
+            {'brand': 'Benz', 'max_speed': 320}
+        ]
+    }
+    print(json.dumps(mydict))
+    try:
+        with open('data.json', 'w', encoding='utf-8') as fs:
+            json.dump(mydict, fs)
+    except IOError as e:
+        print(e)
+    print('保存数据完成!')
+
+
+def json_load():
+    try:
+        with open('data.json', 'r', encoding='utf-8') as fs:
+            print(json.load(fs))
+    except IOError as e:
+        print(e)
+
+
+def request_json():
+    resp = requests.get('https://v1.jinrishici.com/all.json')
+    data_model = json.loads(resp.text)
+    print(data_model)
+    print(data_model['content'])
+
+
+if __name__ == '__main__':
+    request_json()
